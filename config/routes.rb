503b5861e78aa93b841a43fb570308675fdcc87b
takafumi.m <taka.youthful.426@gmail.com>
2018-05-12 10:33:12 +0000

    user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "toppages#index"
-  
+
+  get "signup", to: "users#new"
+  resources :users, only: [:show, :new, :create]
   
   
 end
+
+
+  #6: "signup"の記述＆to: により「/signup」で、users#newアクションを行うことが可能。
+  #7: #show=ユーザーの詳細ページ　#new=新規登録画面用　#create=新規作成
