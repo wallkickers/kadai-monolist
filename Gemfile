707528fe2287eaec5acc71fbfa342a7dbb63969b
takafumi.m <taka.youthful.426@gmail.com>
2018-05-13 04:34:23 +0000

    rakuten api

--- Gemfile
+++ Gemfile
@@ -53,3 +53,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'pry-byebug', group: :development
+
+gem "rakuten_web_service" , "1.5.0"
+gem "dotenv-rails"
---
+++ db/migrate/20180513025946_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.string :code
+      t.string :name
+      t.string :url
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
+
+
+#3: code 商品のID
